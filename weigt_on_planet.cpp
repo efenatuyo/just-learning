26e7af5e0b5e533d83234916614ead60c2703ada
efe <113731512+efenatuyo@users.noreply.github.com>
2023-11-09 14:15:10 +0100

    Update weigt_on_planet.cpp

--- weigt_on_planet.cpp
+++ weigt_on_planet.cpp
@@ -1,38 +1,20 @@
 #include <iostream>
+#include <string>
 
 int main() {
-  double weight;
-  int planet;
-  std::cout << "Your weigt on earth: ";
-  std::cin >> weight;
-  std::cout << "Choose your planet:\n1	Mercury\n2	Venus\n3	Mars\n4	Jupiter\n5	Saturn\n6	Uranus\n7	Neptune\nInput: ";
-  std::cin >> planet;
+  std::string year;
 
-  switch (planet) {
-    case 1:
-      std::cout << "Your weight on Mercury would be: " << weight * 0.38;
-      break;
-    case 2:
-      std::cout << "Your weight on Venus would be: " << weight * 0.91;
-      break;
-    case 3:
-      std::cout << "Your weight on Mars would be: " << weight * 0.38;
-      break;
-    case 4:
-      std::cout << "Your weight on Jupiter would be: " << weight * 2.34;
-      break;
-    case 5:
-      std::cout << "Your weight on Saturn would be: " << weight * 1.06;
-      break;
-    case 6:
-      std::cout << "Your weight on Uranus would be: " << weight * 0.92;
-      break;
-    case 7:
-      std::cout << "Your weight on Neptune would be: " << weight * 1.19;
-      break;
-    default:
-      std::cout << "Invalid planet.";
-      break;
+  std::cout << "Enter your year: ";
+  std::cin >> year;
+
+  if (year.length() == 4) {
+    int yearInt = std::stoi(year);
+    if (yearInt % 4 == 0 && (yearInt % 100 != 0 || yearInt % 400 == 0)) {
+      std::cout << "You have a leap year";
+    } else {
+      std::cout << "You dont have a leap year.";
+    }
+  } else {
+    std::cout << "Invalid length.";
   }
-
 }
