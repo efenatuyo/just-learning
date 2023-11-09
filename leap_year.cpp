6ea48f0caa0b3f9c383489578a3e338109bb6656
efe <113731512+efenatuyo@users.noreply.github.com>
2023-11-09 13:57:45 +0100

    Create leap_year.cpp

---
+++ leap_year.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+int main() {
+  std::string year;
+
+  std::cout << "Enter your year: ";
+  std::cin >> year;
+
+  if (year.length() == 4) {
+    int yearInt = std::stoi(year);
+    if (yearInt % 4 == 0) {
+      std::cout << "You have a leap year";
+    } else {
+      std::cout << "You dont have a leap year.";
+    }
+  } else {
+    std::cout << "Invalid length.";
+  }
+  
+  
+}
