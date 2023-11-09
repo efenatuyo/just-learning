8fb490158c0aacae44f31a6ca78e1477e924244b
efe <113731512+efenatuyo@users.noreply.github.com>
2023-11-09 14:15:35 +0100

    Update leap_year.cpp

--- leap_year.cpp
+++ leap_year.cpp
@@ -9,14 +9,12 @@ int main() {
 
   if (year.length() == 4) {
     int yearInt = std::stoi(year);
-    if (yearInt % 4 == 0) {
+    if (yearInt % 4 == 0 && (yearInt % 100 != 0 || yearInt % 400 == 0)) {
       std::cout << "You have a leap year";
     } else {
       std::cout << "You dont have a leap year.";
     }
   } else {
     std::cout << "Invalid length.";
-  }
-  
-  
+  } 
 }
