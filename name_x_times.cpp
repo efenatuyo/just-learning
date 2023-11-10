f203d4530b2f5d55b535da5b8fe11d0469c76c3f
efe <113731512+efenatuyo@users.noreply.github.com>
2023-11-10 17:04:57 +0100

    Create name_x_times.cpp

---
+++ name_x_times.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+// Define name_x_times() below:
+void name_x_times(std::string name, int amount) {
+  while (amount > 0) {
+    std::cout << name << "\n";
+    amount--;
+  }
+}
+
+int main() {
+  
+  std::string my_name = "Add your name here!";
+  int some_number = 5; // Change this if you like!
+  // Call name_x_times() below with my_name and some_number
+  name_x_times(my_name, some_number);
+  
+}
